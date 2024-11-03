63aa28037fd8f9c411f5e744c99287ac53f0bf9f
lodjim <lodjim5@gmail.com>
2024-11-03 14:15:23 +0000

    first commit

---
+++ lua/plugins.lua
@@ -0,0 +1,11 @@
+return {
+  -- Add the plugins to this table
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "stevearc/dressing.nvim", name = "dressing.nvim" },
+  { "folke/lazy.nvim", name = "lazydev.nvim" }, -- Ensure this matches the plugin's repository name
+  {
+    "craftzdog/solarized-osaka.nvim",
+    name = "solarized-osaka.nvim",
+    branch = "osaka", -- Mention the branch if required
+  },
+}
